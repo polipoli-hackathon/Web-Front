c8081965ee87ef938b3b339c07324b17ae865f67
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2019-04-04 16:40:53 +0900

    [Update] 各サービス・コンポーネントを追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { LineAuthHandlerComponent } from './components/line-auth-handler/line-auth-handler.component';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    LineAuthHandlerComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/components/line-auth-handler/line-auth-handler.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-line-auth-handler',
+  templateUrl: './line-auth-handler.component.html',
+  styleUrls: ['./line-auth-handler.component.scss']
+})
+export class LineAuthHandlerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
