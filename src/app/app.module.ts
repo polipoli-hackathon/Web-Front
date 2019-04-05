a996e4b0ad78c2e72038b6a55d0ccc69eb7c0c79
Ryusei Kurata <amnosmusic13@gmail.com>
2019-04-05 16:22:33 +0900

    [update]input-eventPage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventListComponent } from './pages/event-list/event-list.component';
 import { EventListItemComponent } from './pages/event-list-item/event-list-item.component';
+import { InputEventComponent } from './pages/input-event/input-event.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventListComponent,
-    EventListItemComponent
+    EventListItemComponent,
+    InputEventComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/input-event/input-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-input-event',
+  templateUrl: './input-event.component.html',
+  styleUrls: ['./input-event.component.scss']
+})
+export class InputEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
