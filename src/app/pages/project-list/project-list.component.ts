e258fee216f232c1fa33c571875c002a57a646e2
Ryusei Kurata <amnosmusic13@gmail.com>
2019-04-06 16:40:42 +0900

    [update]project-listのロジック

--- src/app/pages/project-list/project-list.component.ts
+++ src/app/pages/project-list/project-list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Project } from 'src/app/models/project';
+import { ProjectService } from 'src/app/services/project.service';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-project-list',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-list.component.scss']
 })
 export class ProjectListComponent implements OnInit {
+  public projects$: Observable<Project[]>;
 
-  constructor() { }
+  constructor(
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.get();
+  }
+
+  private get(): Observable<Project[]> {
+    this.projects$ = this.projectService.getProjects();
+    return this.projects$;
+  }
+
+  apply(data: Project): void {
+    this.projectService.applyProject(data);
+  }
+
+  cancel(data: Project): void {
+    this.projectService.cancelProject(data);
   }
 
 }
