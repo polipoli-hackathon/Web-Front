9613098168d07c98a8ce09b0d52726016ad4f468
Ryusei Kurata <amnosmusic13@gmail.com>
2019-04-07 23:39:09 +0900

    [update]routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { InputProjectComponent } from './pages/input-project/input-project.component';
+import { ProjectListItemComponent } from './components/project-list-item/project-list-item.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: '/input-project', component: InputProjectComponent }
+  { path: '/input-project', component: InputProjectComponent },
+  { path: '/project-list-item', component: ProjectListItemComponent }
   // {path: 'line_auth_handler', component: LineAuthHandlerComponent},
 ];
 
